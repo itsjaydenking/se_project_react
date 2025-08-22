dfd57175b2ebe69e8e0e0d3e8e876a323000a46c
Jayden King <itsjaydenking@gmail.com>
2025-08-21 17:52:06 -0700

    Implement ItemCard component with clothing items and styles

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,11 +1,25 @@
 import "./ItemCard.css";
+import { defaultClothingItems } from "../../utils/constants.js";
 
 function ItemCard() {
   return (
-    <div className="item-card">
-      <img src="https://via.placeholder.com/150" alt="Item" />
-      <h3>Item Name</h3>
-    </div>
+    <section className="item-cards">
+      <p className="item-cards__text">
+        Today is 75&deg;F / You may want to wear:
+      </p>
+      <ul className="item-cards__list">
+        {defaultClothingItems.map((item) => (
+          <li className="item-cards__item" key={item._id}>
+            <p className="item-cards__name">{item.name}</p>
+            <img
+              className="item-cards__image"
+              src={item.link}
+              alt={item.name}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,11 +6,7 @@ function Main() {
   return (
     <main>
       <WeatherCard />
-      <section className="cards">
-        <p className="cards__text">
-          Today is ## &deg; F / You may want to wear:
-        </p>
-      </section>
+
       <ItemCard />
     </main>
   );
