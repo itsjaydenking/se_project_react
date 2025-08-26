2f20e51c34a79cfcf207b3c094f1a9437537b48c
Jayden King <itsjaydenking@gmail.com>
2025-08-26 13:56:48 -0700

    Implement modal functionality for adding garments: update App and Header components, create ModalWithForm with input fields and styles, and add close icon.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,15 +8,90 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
 // import ItemModal from "../ItemModal/ItemModal.jsx";
 
 function App() {
-  const [weatherData, setWeatherData] = useState({ type: "hot" });
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+  const [activeModal, setActiveModal] = useState("");
+
+  const handleAddClick = () => {
+    setActiveModal("modal_opened");
+  };
+
+  const handleCloseClick = () => {
+    setActiveModal("");
+  };
+
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
+        <Header handleAddClick={handleAddClick} />
         <Main weatherData={weatherData} />
-        <Footer />
-        <ModalWithForm />
+
+        <ModalWithForm
+          buttonText="Add Garment"
+          title="New Garment"
+          activeModal={activeModal}
+          handleCloseClick={handleCloseClick}
+        >
+          <label className="modal__label" htmlFor="name">
+            Name{" "}
+          </label>
+          <input
+            className="modal__input"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter garment name"
+            required
+          />
+          <label className="modal__label" htmlFor="imageUrl">
+            Image{" "}
+          </label>
+          <input
+            className="modal__input"
+            type="url"
+            id="imageUrl"
+            name="imageUrl"
+            placeholder="Enter image URL"
+            required
+          />
+          <fieldset className="modal__radio-buttons">
+            <legend className="modal__legend">Select the weather type</legend>
+            <label
+              htmlFor="hot"
+              className="modal__label modal__label_type_radio"
+            >
+              <input
+                id="hot"
+                type="radio"
+                className="modal__radio-input modal__input_type_radio"
+              />{" "}
+              Hot
+            </label>
+            <label
+              htmlFor="warm"
+              className="modal__label modal__label_type_radio"
+            >
+              <input
+                id="warm"
+                type="radio"
+                className="modal__radio-input modal__input_type_radio"
+              />{" "}
+              Warm
+            </label>
+            <label
+              htmlFor="cold"
+              className="modal__label modal__label_type_radio"
+            >
+              <input
+                id="cold"
+                type="radio"
+                className="modal__radio-input modal__input_type_radio"
+              />{" "}
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
         {/* <ItemModal />  */}
+        <Footer />
       </div>
     </div>
   );
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,5 +1,33 @@
-function ModalWithForm() {
-  return <div>ModalWithForm</div>;
+import closeButton from "../../assets/images/close-icon.svg";
+import "./ModalWithForm.css";
+
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  handleCloseClick,
+}) {
+  return (
+    <div className={`modal ${activeModal ? "modal_opened" : ""}`}>
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button
+          className="modal__close"
+          type="button"
+          onClick={handleCloseClick}
+        >
+          <img src={closeButton} alt="Close" />
+        </button>
+        <form action="" className="modal__form">
+          {children}
+          <button className="modal__submit" type="submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ModalWithForm;
