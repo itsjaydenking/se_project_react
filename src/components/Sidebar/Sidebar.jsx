340aeeaf42cea9bac8cf8a3103c37d04a829c09e
Jayden King <itsjaydenking@gmail.com>
2025-09-24 09:16:42 -0700

    Refactor layout and styles for Header, Footer, Profile, ClothesSection, and Sidebar components; enhance responsiveness and improve user interaction elements

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,8 +4,26 @@ import avatar from "../../assets/images/avatar.svg";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <img src={avatar} alt="User Avatar" className="sidebar__avatar" />
-      <p className="sidebar__username">Username</p>
+      <div className="sidebar__top">
+        <img src={avatar} alt="User Avatar" className="sidebar__avatar" />
+        <p className="sidebar__username">Username</p>
+      </div>
+      <div className="sidebar__actions">
+        <button
+          type="button"
+          className="sidebar__btn"
+          onClick={() => console.log("Change Profile Data clicked")}
+        >
+          Change Profile Data
+        </button>
+        <button
+          type="button"
+          className="sidebar__btn sidebar__btn_logout"
+          onClick={() => console.log("Log Out clicked")}
+        >
+          Log Out
+        </button>
+      </div>
     </div>
   );
 }
